6d8dc59544a7591c079c2097e2be21c19799fc4e
Spencer Frost <s.s.frost@gmail.com>
2018-10-19 15:39:33 -0400

    Finalize router and routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
+import Portfolio from './views/Portfolio.vue'
+import Contact from './views/Contact.vue'
 
 Vue.use(Router)
 
@@ -8,18 +11,9 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    { path: '/', component: Home },
+    { path: '/portfolio', component: Portfolio},
+    { path: '/about', component: About},
+    { path: '/contact', component: Contact}
   ]
 })
