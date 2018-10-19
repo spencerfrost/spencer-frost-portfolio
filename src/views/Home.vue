93b84d429c4f4b76ce0930cc7a0297b928ca2081
Spencer Frost <s.s.frost@gmail.com>
2018-10-19 15:43:51 -0400

    Update Home style and content. Add Contact view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/spencer.png">
-    <h1>Spencer Frost</h1>
+    <img alt="Vue logo" src="../assets/spencer.png" class="pic">
+    <div class="heading">
+      <h1>Spencer <strong class="yellow">Frost</strong></h1>
+      <h2>Front-end Developer / Wordpress Dabbler</h2>
+    </div>
   </div>
 </template>
 
@@ -11,3 +14,20 @@ export default {
   name: 'home'
 }
 </script>
+
+<style>
+  .heading {
+    float: left;
+    text-align: left;
+  }
+  .heading h1 {
+    font-size: 4rem;
+    font-family: 'Chakra Petch', sans-serif;
+  }
+  .yellow {
+    color: #FFC636;
+  }
+  .heading h2 {
+    background-color: rgba(50, 50, 50, 0.3);
+  }
+</style>
