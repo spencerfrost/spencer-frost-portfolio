077174038e5d577896f5022985721dd8000292c2
Spencer Frost <s.s.frost@gmail.com>
2018-10-19 13:53:37 -0400

    Initial commit. Ready for content

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <app-header></app-header>
+    <br>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+  components: {
+    appHeader: Header
+  }
+}
+</script>
+
 
 <style>
 #app {
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <ul class="nav justify-content-center">
+        <router-link 
+            to="/"
+            tag="li"
+            class="nav-item active"
+            ><a class="nav-link">Home</a>
+        </router-link>
+        <router-link 
+            to="/About"
+            tag="li"
+            class="nav-item"
+            ><a class="nav-link">About</a>
+        </router-link>
+        <router-link 
+            to="/Portfolio"
+            tag="li"
+            class="nav-item"
+            ><a class="nav-link">Portfolio</a>
+        </router-link>
+        <router-link 
+            to="/Contact"
+            tag="li"
+            class="nav-item"
+            ><a class="nav-link">Contact</a>
+        </router-link>
+    </ul>
+<!--     
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+
+    <!-- <ul class="navbar-nav mr-auto">
+        <router-link 
+            to="/"
+            tag="li"
+            class="nav-item active"
+            ><a class="nav-link">Home</a>
+        </router-link>
+        <router-link 
+            to="/About"
+            tag="li"
+            class="nav-item"
+            ><a class="nav-link">About</a>
+        </router-link>
+        <router-link 
+            to="/Portfolio"
+            tag="li"
+            class="nav-item"
+            ><a class="nav-link">Portfolio</a>
+        </router-link>
+    </ul> -->
+</template>
