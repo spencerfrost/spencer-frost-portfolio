33e07e8b3f7176cb1e4e7cf1cfa686fb4dbf3274
Spencer Frost <s.s.frost@gmail.com>
2018-10-19 15:41:53 -0400

    Add custom nav styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,46 @@
 <template>
-    <ul class="nav justify-content-center">
+    <ul class="nav nav-pills">
         <router-link 
             to="/"
             tag="li"
-            class="nav-item active"
-            ><a class="nav-link">Home</a>
+            class="nav-item"
+            ><a class="nav-link active" data-toggle="pill" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
         </router-link>
         <router-link 
-            to="/About"
+            to="/about"
             tag="li"
             class="nav-item"
-            ><a class="nav-link">About</a>
+            ><a class="nav-link" data-toggle="pill" role="tab" aria-controls="pills-home" aria-selected="false">About</a>
         </router-link>
         <router-link 
-            to="/Portfolio"
+            to="/portfolio"
             tag="li"
             class="nav-item"
-            ><a class="nav-link">Portfolio</a>
+            ><a class="nav-link" data-toggle="pill" role="tab" aria-controls="pills-home" aria-selected="false">Portfolio</a>
         </router-link>
         <router-link 
-            to="/Contact"
+            to="/contact"
             tag="li"
             class="nav-item"
-            ><a class="nav-link">Contact</a>
+            ><a class="nav-link" data-toggle="pill" role="tab" aria-controls="pills-home" aria-selected="false">Contact</a>
         </router-link>
     </ul>
 </template>
+
+<style>
+    .nav-link{
+        color: #2c3e50;
+    }
+    .nav-pills li a {
+        border: 1px solid #2c3e50;
+        margin: 0 5px;
+        background-color: #FF644461 !important;
+    }
+    .nav-pills li .active{
+        background-color: #FF6444B3 !important;
+    }
+    .nav-link:hover {
+        color: #FF6444;
+    }
+</style>
+
