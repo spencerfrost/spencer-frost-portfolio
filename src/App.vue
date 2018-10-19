d8d00a75e97347bc4c31235f4d31d1a2923f2b1e
Spencer Frost <s.s.frost@gmail.com>
2018-10-19 15:37:54 -0400

    Add background style

--- src/App.vue
+++ src/App.vue
@@ -24,17 +24,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  background-color: #00ADA9;
+  background-image: url(./assets/3px-tile.png);
+  height: 100vh;
+  padding: 20px;
 }
 </style>
