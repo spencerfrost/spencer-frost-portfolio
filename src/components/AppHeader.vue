5fc1c8f60d64f6feaaf1e440cd5877ebe5d85d63
Spencer Frost <34923439+spencerfrost@users.noreply.github.com>
2024-07-04 11:31:15 -0400

    Update website to Vue 3 and Vite

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <HeaderBranding />
+    <HeaderNav />
+  </header>
+</template>
+
+<script>
+import HeaderBranding from './AppHeader/HeaderBranding.vue'
+import HeaderNav from './AppHeader/HeaderNav.vue'
+
+export default {
+  components: {
+    HeaderBranding,
+    HeaderNav,
+  },
+}
+</script>
+
+<style scoped>
+.header {
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
