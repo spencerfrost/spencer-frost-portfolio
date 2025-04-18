f3b8586f941ba00036638a3c34d9a92e32e5cfc0
spencerfrost <s.s.frost@gmail.com>
2025-04-18 01:39:23 -0400

    feat: implement theme switcher component with welcome dialog and theme selection

---
+++ components/theme-switcher/ThemeSwitcherDialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <Dialog>
+    <DialogTrigger as-child>
+      <template #default>
+        <Button v-if="icon" variant="ghost" size="icon">
+          <Icon name="lucide-lab:coffee" class="h-5 w-5" />
+        </Button>
+        <Button v-else>Open Theme Switcher</Button>
+      </template>
+    </DialogTrigger>
+    <DialogContent class="p-0 border-none h-64 w-max-screen sm:w-128">
+      <ThemeSwitcher class="flex flex-col gap-4 p-4" />
+    </DialogContent>
+  </Dialog>
+</template>
+
+<script lang="ts" setup>
+  import {
+  defineProps
+} from 'vue';
+
+  const props = defineProps({
+    icon: {
+      type: Boolean,
+      default: false
+    }
+  });
+</script>
