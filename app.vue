c153534b08d6bd7f925b66d1d961dc7b81478c66
spencerfrost <s.s.frost@gmail.com>
2025-05-01 14:31:35 -0400

    feat: add script setup for head management with body attributes in app.vue

--- app.vue
+++ app.vue
@@ -3,3 +3,11 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: 'text-foreground bg-background transition-colors -z-20'
+  }
+})
+</script>
