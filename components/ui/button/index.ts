3d82f2d43856a59bce8779586fbf77ee964ed0b0
spencerfrost <s.s.frost@gmail.com>
2025-05-01 14:29:52 -0400

    feat: enhance button variants with additional color options for improved styling

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -26,13 +26,41 @@ export const buttonVariants = cva(
     variants: {
       variant: {
         default:
+          "bg-card text-foreground shadow hover:bg-card/70",
+        primary:
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        rosewater:
+          "bg-rosewater text-semantic-foreground shadow-sm hover:bg-rosewater/80",
+        flamingo:
+          "bg-flamingo text-semantic-foreground shadow-sm hover:bg-flamingo/80",
+        pink:
+          "bg-pink text-semantic-foreground shadow-sm hover:bg-pink/80",
+        mauve:
+          "bg-mauve text-semantic-foreground shadow-sm hover:bg-mauve/80",
+        red:
+          "bg-red text-semantic-foreground shadow-sm hover:bg-red/80",
+        peach:
+          "bg-peach text-semantic-foreground shadow-sm hover:bg-peach/80",
+        yellow:
+          "bg-yellow text-semantic-foreground shadow-sm hover:bg-yellow/80",
+        green:
+          "bg-green text-semantic-foreground shadow-sm hover:bg-green/80",
+        teal:
+          "bg-teal text-semantic-foreground shadow-sm hover:bg-teal/80",
+        sky:
+          "bg-sky text-semantic-foreground shadow-sm hover:bg-sky/80",
+        sapphire:
+          "bg-sapphire text-semantic-foreground shadow-sm hover:bg-sapphire/80",
+        blue:
+          "bg-blue text-semantic-foreground shadow-sm hover:bg-blue/80",
+        lavender:
+          "bg-lavender text-semantic-foreground shadow-sm hover:bg-lavender/80",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:text-secondary",
         link: "text-primary underline-offset-4 hover:underline",
       },
