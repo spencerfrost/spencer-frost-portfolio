0c07e679ec5988d790b8f16f3d0f458048e2deeb
spencerfrost <s.s.frost@gmail.com>
2025-05-01 14:32:51 -0400

    Refactor code structure for improved readability and maintainability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import vue from 'eslint-plugin-vue'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    plugins: { vue },
+    rules: {
+      // Enforce multi-line if more than 3 attributes
+      'vue/max-attributes-per-line': ['error', {
+        singleline: 3,
+        multiline: 1,
+      }],
+    },
+  }
 )
